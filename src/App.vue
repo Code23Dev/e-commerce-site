8827cd8e49499aeae55033afa3989e4a8bcc762a
leman ibrahimli <65496038+Code23Dev@users.noreply.github.com>
2021-07-08 21:58:37 +0400

    vue code

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="data">
+    <v-app-bar app dense flat outlined>
+      <v-tabs class="navbar-class">
+        <v-tab to="/" style="color:white;">Home</v-tab>
+        <v-tab to="/products" style="color:white;">Products</v-tab>
+        <v-tab to="/cart" style="color:white;">Cart</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<style lang="css">
+  #data{
+    background-color:black;
+    color:white;
+  }
+  .navbar-class{
+    background-color:black;
+  }
+  .navbar-class-0{
+     color:white ;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1 class="home-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aliquid
+      impedit quod voluptatibus maiores provident, perspiciatis laborum.
+      Molestias exercitationem quod ut aspernatur nostrum debitis molestiae
+      ipsam, dolores assumenda alias quidem.
+    </h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+<style lang="css">
+.home-text{
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 56px;
+  color:#5A2E1B;
+}
+</style>
